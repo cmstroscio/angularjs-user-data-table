fed10930155303fcc01a10c3708f209d45f71242
marshall <marshals@netapp.com>
2017-06-11 00:28:07 -0400

    new message

--- app/app.js
+++ app/app.js
@@ -1,14 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+    $http.get("http://jsonplaceholder.typicode.com/users")
+    .then(function(response) {
+        $scope.users = response.data;
+    });
+});
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
---
+++ app/controllers.js
@@ -0,0 +1,9 @@
+// var app = angular.module("MyApp", []);
+
+// app.controller("PostsCtrl", function($scope, $http) {
+//   $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
+//   $http.get('http://jsonplaceholder.typicode.com/users').
+//     success(function(data, status, headers, config) {
+//       $scope.posts = data;
+//     });
+// });
