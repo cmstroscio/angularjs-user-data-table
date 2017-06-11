47de5bd25895ba150deb1bdeb4409fa1cb7e2ec6
marshall <marshals@netapp.com>
2017-06-11 11:17:37 -0400

    set up ng href, filter

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ["ngRoute"]);
 app.controller('myCtrl', function($scope, $http) {
     $http.get("http://jsonplaceholder.typicode.com/users")
     .then(function(response) {
         $scope.users = response.data;
     });
+    $scope.getIndex = function(users) {
+    return $scope.users.indexOf(name);
+}
 });
 
