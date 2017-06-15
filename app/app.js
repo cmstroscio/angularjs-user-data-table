ea0b2d878cf39585c021ef883b22e309535ce104
marshall <marshals@netapp.com>
2017-06-14 22:10:03 -0400

    clean up index, js

--- app/app.js
+++ app/app.js
@@ -2,13 +2,50 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', ["ngRoute"]);
-app.controller('myCtrl', function($scope, $http) {
+
+  app.config(function($routeProvider, $locationProvider) {
+
+
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.htm",
+    })
+    .when("/#/5", {
+        templateUrl : "chelsey.html",
+        controller: "myCtrl", 
+    })
+    .when("/green", {
+        templateUrl : "green.htm"
+    })
+    .when("/blue", {
+        templateUrl : "blue.htm"
+    });
+});
+
+
+app.controller('myCtrl', function($scope, $http, $route, $routeParams, $location) {
+    
+    // needed?
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+
     $http.get("http://jsonplaceholder.typicode.com/users")
     .then(function(response) {
         $scope.users = response.data;
+
+        // needed?
+        // $scope.profile= $routeParams.id;
+
+
     });
-    $scope.getIndex = function(users) {
-    return $scope.users.indexOf(name);
-}
+    // needed?
+    // $scope.getIndex = function(users) {
+    // return $scope.users.indexOf(id);
+    // }
+
+
+
+
 });
 
